ea8d72bea2e3207b3f38748a919b2fbd5d9adab2
AnishGrg <56734898+AnishGrg@users.noreply.github.com>
2021-12-27 13:01:15 +0545

    This is a first commit

---
+++ ProdCat/Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ProdCat.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProdCat.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        NobelDBEntities db = new NobelDBEntities();
+
+
+        public ActionResult ManageCategory()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            return View(db.tblCategories.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tblCategory category)
+        {
+            db.tblCategories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var data = db.tblCategories.Find(id);
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(tblCategory tbl)
+        {
+            var data = db.tblCategories.Find(tbl.CategoryID);
+            data.CategoryName = tbl.CategoryName;
+            db.Entry(data).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            tblCategory category = db.tblCategories.Find(id);
+            return View(category);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            tblCategory data = db.tblCategories.Find(id);
+            db.tblCategories.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ ProdCat/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using ProdCat.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProdCat.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        NobelDBEntities db = new NobelDBEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ManageProduct()
+        {
+            return RedirectToAction("List");
+        }
+
+        public ActionResult List()
+        {
+            return View(db.tbProducts.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.tblCategories.ToList(), "CategoryId", "CategoryName");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(tbProduct product)
+        {
+            db.tbProducts.Add(product);
+
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var data = db.tbProducts.Find(id);
+            ViewBag.CategoryID = new SelectList(db.tbProducts.ToList(), "CategoryID", "CategoryName", data.CategoryID);
+            return View(data);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(tbProduct tb)
+        {
+            var data = db.tbProducts.Find(tb.ProductID);
+                data.ProductName = tb.ProductName;
+                data.UnitPrice = tb.UnitPrice;
+                data.SellingPrice = tb.SellingPrice;
+            db.Entry(data).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("List");
+
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            tbProduct tb = db.tbProducts.Find(id);
+            return View(tb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            tbProduct tb = db.tbProducts.Find(id);
+            db.tbProducts.Remove(tb);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+    }
+}
